d937b5b28eac78529c31330ee116c9c3d25a1714
kamel <k.moussaoui@simply-permis.com>
2020-09-22 12:33:36 +0200

    navbar et typed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,7 +25,7 @@ require("channels")
 // External imports
 import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner';
-
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
@@ -33,4 +33,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   loadDynamicBannerText();
+  initUpdateNavbarOnScroll();
 });
